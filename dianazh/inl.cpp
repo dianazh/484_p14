89fc769e7cd8d46d01d6a35bf4c9672d0e40cda5
dianazh <dianazh@umich.edu>
2014-04-11 17:44:40 -0400

    new join.cpp (order of index) and inl.cpp

--- dianazh/inl.cpp
+++ dianazh/inl.cpp
@@ -16,10 +16,24 @@ Status Operators::INL(const string& result,           // Name of the output rela
                       const AttrDesc& attrDesc2,      // The left attribute in the join predicate
                       const int reclen)               // Length of a tuple in the output relation
 {
-  cout << "Algorithm: Indexed NL Join" << endl;
+    cout << "Algorithm: Indexed NL Join" << endl;
 
-  /* Your solution goes here */
+    /* Your solution goes here */
+    Status status;
+    RID rid_old1;
+    Record rec_old1;
+    HeapFileScan table1(attrDesc1->relName, status);
+    if (status != OK) return status;
 
-  return OK;
+    RID rid_old2;
+    Record rec_old2;
+    Index index(attrDesc2->relName, attrDesc2->attrOffset, attrDesc2->attrLen, (Datatype)
+            attrDesc2->attrType, 0, status);
+    if (status != OK) return status;
+    HeapfileScan table2(attrDesc2->relName, status);
+    if (status != OK) return status;
+
+
+    return OK;
 }
 
