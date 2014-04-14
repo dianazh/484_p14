491f163c43570f3b974282ed96a37c0b83b4d9e9
dianazh <dianazh@umich.edu>
2014-04-14 02:17:09 -0400

    page finish

--- dianazh/page.cpp
+++ dianazh/page.cpp
@@ -11,6 +11,14 @@ using namespace std;
 void Page::init(int pageNo)
 {
     /* Solution Here */
+    //cout<<"--------------init"<<endl; //test
+    slotCnt = 0;
+    freePtr = 0;
+    freeSpace = PAGESIZE - DPFIXED;
+    prevPage = 0; //?
+    nextPage = 0; //?
+    curPage = pageNo;
+    //cout<<"pageid: "<<curPage<<" slotCnt: "<<slotCnt<<endl; //test
 }
 
 // dump page utlity
@@ -49,6 +57,7 @@ const int Page::getNextPage() const
 const short Page::getFreeSpace() const
 {
     /* Solution Here */
+    return freeSpace;
 }
     
 // Add a new record to the page. Returns OK if everything went OK
@@ -60,7 +69,43 @@ const short Page::getFreeSpace() const
 
 const Status Page::insertRecord(const Record & rec, RID& rid)
 {
+    //slotCnt is (-inf, -1,0]
+    //slotNo is 0,1,2,3 -> new slotNo = -slotCnt, slotCnt --
     /* Solution Here */
+    //cout<<"--------------insertRecord"<<rid.slotNo<<endl; //test
+    int slot_cand = 1;  //index: <= 0
+    if (freeSpace < rec.length){
+        return NOSPACE;
+    } else {
+        //old slot exist
+        for (int i=0; i>slotCnt; i--){
+            if (slot[i].length == -1){
+                slot_cand =  i;
+                break;
+            }
+        }
+        if ((slot_cand) == 1 && (freeSpace < (rec.length+sizeof(slot_t)))){
+            return NOSPACE;
+        }
+    }
+    //new slot
+    if (slot_cand == 1){
+        slot_cand = slotCnt;
+        freeSpace -= sizeof(slot_t);
+        slotCnt --;
+    }
+    //update slotNo
+    rid.slotNo = -slot_cand;
+    rid.pageNo = curPage;
+    //cout<<"--------------insertRecord slotNo: "<<rid.slotNo<<endl; //test
+    slot[slot_cand].length = rec.length;
+    slot[slot_cand].offset = freePtr;
+    freePtr += rec.length;
+    freeSpace -= rec.length;
+    memcpy(data+slot[slot_cand].offset,rec.data, rec.length);
+    //cout<<"pageid: "<<curPage<<" slotCnt: "<<slotCnt<<endl; //test
+    //dumpPage(); //test
+    return OK;
 }
 
 
@@ -72,6 +117,42 @@ const Status Page::insertRecord(const Record & rec, RID& rid)
 const Status Page::deleteRecord(const RID & rid)
 {
     /* Solution Here */
+    //cout<<"--------------deleteRecord rid.slotNo: "<<rid.slotNo<<endl; //test
+    
+    if (rid.pageNo != curPage){
+        return INVALIDSLOTNO;
+    }else if (rid.slotNo >= (-slotCnt)){
+        return INVALIDSLOTNO;
+    }  else if (slot[-rid.slotNo].length == -1){
+        return INVALIDSLOTNO;
+    }
+    
+    //compact data
+    int steplen = slot[-rid.slotNo].length;
+    int currOffset = slot[-rid.slotNo].offset;
+    int originOffset = currOffset;
+
+    freeSpace += slot[-rid.slotNo].length;
+    freePtr -= steplen;
+    slot[-rid.slotNo].length = -1;
+    if (freePtr == 0){
+        return NORECORDS;
+    }
+    
+    while (currOffset < freePtr){
+        bcopy(data+currOffset+steplen, data+currOffset, steplen);
+        currOffset += steplen;
+    }
+    
+    for (int i=0; i>slotCnt; i--){
+        if ((slot[i].offset > originOffset)&&(slot[i].length != -1)){
+            slot[i].offset -= steplen;
+        }
+    }
+    //slotCnt++;  //slot# never decrease
+    
+    //cout<<"pageid: "<<curPage<<" slotCnt: "<<slotCnt<<endl; //test
+    return OK;
 }
 
 // returns RID of first record on page
@@ -79,6 +160,28 @@ const Status Page::deleteRecord(const RID & rid)
 const Status Page::firstRecord(RID& firstRid) const
 {
     /* Solution Here */
+    //cout<<"--------------firstRecord"<<endl; //test
+    //cout<<"pageid: "<<curPage<<" slotCnt: "<<slotCnt<<endl; //test
+    //dumpPage();  //test
+    //what is first rec? offset = 1? +++ or first slot? 
+    //in page offset
+    /*for (int i=0; i>slotCnt; i--){
+        if ((slot[i].length!=-1)&&(slot[i].offset==0)){
+            firstRid.pageNo = curPage;
+            firstRid.slotNo = -i;
+            return OK;
+        }
+    }*/
+    
+    //in slot[]
+    for (int i=0; i>slotCnt; i--){
+        if (slot[i].length != -1){
+            firstRid.pageNo = curPage;
+            firstRid.slotNo = -i;
+            return OK;
+        }
+    }
+    return NORECORDS;
 }
 
 // returns RID of next record on the page
@@ -86,6 +189,38 @@ const Status Page::firstRecord(RID& firstRid) const
 const Status Page::nextRecord (const RID &curRid, RID& nextRid) const
 {
     /* Solution Here */
+    //cout<<"--------------nextRecord"<<endl; //test
+    //cout<<"pageid: "<<curPage<<" slotCnt: "<<slotCnt<<endl; //test
+    //dumpPage();  //test
+    //in page offset
+    int currOffset = slot[-curRid.slotNo].offset;
+    int steplen = slot[-curRid.slotNo].length;
+    /*for (int i=0; i>slotCnt; i--){
+        if (slot[i].offset == currOffset + steplen){
+            nextRid.pageNo = curRid.pageNo;
+            nextRid.slotNo = -i;
+            return OK;
+        }
+    }*/
+    //check: curRid valid? 
+    //in slot[]
+    int curslot = curRid.slotNo;
+    if (curRid.slotNo >= (-slotCnt)){
+        return ENDOFPAGE;
+    } else {
+        curslot++;
+        while (curslot!=(-slotCnt)){
+            if (slot[-curslot].length != -1){
+                nextRid.pageNo = curRid.pageNo;
+                nextRid.slotNo = curslot;
+                return OK;
+            }
+            curslot++;
+        }
+        return ENDOFPAGE;
+    }
+    
+    return ENDOFPAGE;
 }
 
 // returns length and pointer to record with RID rid
@@ -93,4 +228,29 @@ const Status Page::nextRecord (const RID &curRid, RID& nextRid) const
 const Status Page::getRecord(const RID & rid, Record & rec)
 {
     /* Solution Here */
+    //cout<<"--------------getRecord slotNo: "<<rid.slotNo<<endl; //test
+    //cout<<"pageid: "<<curPage<<" slotCnt: "<<slotCnt<<endl; //test
+    //dumpPage();//test
+    if (rid.pageNo != curPage){
+        //cout<<"case 1"<<endl; //test
+        /*cout<<"slotno: "<<rid.slotNo<<" offset: "<<
+            slot[-rid.slotNo].offset<<" "<<slot[-rid.slotNo].length<<endl; //test*/
+        return INVALIDSLOTNO;
+    } else if (rid.slotNo >= (-slotCnt)){
+        /*cout<<"case 2"<<endl; //test
+        cout<<"slotno: "<<rid.slotNo<<" offset: "<<
+            slot[-rid.slotNo].offset<<" "<<slot[-rid.slotNo].length<<endl; //test*/
+        return INVALIDSLOTNO;
+    } else if (slot[-rid.slotNo].length == -1){
+        /*cout<<"case 3"<<endl; //test
+        cout<<"slotno: "<<rid.slotNo<<" offset: "<<
+            slot[-rid.slotNo].offset<<" "<<slot[-rid.slotNo].length<<endl; //test*/
+        return INVALIDSLOTNO;
+    }
+    rec.length = slot[-rid.slotNo].length;
+    //cout<<"slotno: "<<rid.slotNo<<" offset: "<<
+    //    slot[-rid.slotNo].offset<<" "<<slot[-rid.slotNo].length<<endl; //test
+    //memcpy(rec.data, (data+slot[rid.slotNo].offset), rec.length);
+    rec.data = (void*) data+slot[-rid.slotNo].offset;
+    return OK;
 }
